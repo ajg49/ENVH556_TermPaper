5eb22e0f8bc68014643dd9535a9eee7503cba4db
Katie Webb <krwebb@uw.edu>
2024-10-20 04:30:22 +0000

    test

--- UntitledR.R
+++ UntitledR.R
@@ -12,3 +12,10 @@ usethis::use_git_config(user.name="callankh", user.email="callak2@uw.edu")
 
 
 >>>>>>> 2ab5acc70910ecec42d881b011ec3594a40ab7b6
+
+
+Katie Testing
+
+frogs <- c(1, 2, 3, 4, 5, 6)
+
+print(frogs)
